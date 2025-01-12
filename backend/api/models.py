f30fff51753646817609147c5347da788fb55008
Илья Екимов <umilja140608@gmail.com>
2025-01-13 00:13:25 +0300

    3.1.1

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.utils import timezone
 
 from .constants import (INGREDIENT_NAME_MAX_LENGTH,
                         MEASUREMENT_UNIT_MAX_LENGTH, MIN_AMOUNT_VALUE,
