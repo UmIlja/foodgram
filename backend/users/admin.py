ed926531e235017acb3fe7916b79ec09ead9e221
Илья Екимов <umilja140608@gmail.com>
2025-01-15 00:29:30 +0300

    4.24

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -33,8 +33,7 @@ class IngredientInline(admin.StackedInline):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-
-    list_display = ('name', 'get_image', 'author', 'in_favourite_count')
+    list_display = ('name', 'get_image', 'author')
     list_select_related = ('author',)
     list_filter = ['tags']
     search_fields = ('name', 'author__username')
@@ -49,10 +48,26 @@ class RecipeAdmin(admin.ModelAdmin):
             return mark_safe(
                 f'<img src={obj.image.url} width="50" height="60" />')
 
+    def get_readonly_fields(self, request, obj=None):
+        """Добавляем поле для отображения количества добавлений в избранное на странице редактирования."""
+        if obj:  # Если это страница редактирования
+            return super().get_readonly_fields(request, obj) + ('in_favourite_count',)
+        return super().get_readonly_fields(request, obj)
+
     @admin.display(description='Число добавлений в избранное')
     def in_favourite_count(self, obj):
         """Возвращает количество добавлений рецепта в избранное."""
         return obj.favorite_items.count()
+
+    def in_favourite_count_display(self, obj):
+        """Метод для отображения количества добавлений в избранное."""
+        return self.in_favourite_count(obj)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        """Добавляем контекст для отображения количества добавлений в избранное."""
+        extra_context = extra_context or {}
+        extra_context['in_favourite_count'] = self.in_favourite_count_display(self.get_object(request, object_id))
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
 
 
 @admin.register(Ingredient)
